7cc11234af4dd7237ed3fd4f1ff97a7b707e4b2f
Unknown <u201714978@upc.edu.pe>
2018-08-28 03:46:24 -0500

    I'M SO TIRED
    ME MUERO DE SUEÑO CSMR

--- app/src/main/java/com/algebralineal/tpalgebralineal/MainActivity.java
+++ app/src/main/java/com/algebralineal/tpalgebralineal/MainActivity.java
@@ -1,23 +1,26 @@
-package com.algebralineal.tfalgebralineal;
+package com.algebralineal.tpalgebralineal;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import com.algebralineal.tpalgebralineal.R;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void goToEncodeActivity(View view)
+    public void goToEncodeActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, EncodeActivity.class);
+        startActivity(intent);
+    }
+
+    public  void goToInformationActivity(View view)
     {
-        Intent intent = new Intent(this, EncodeActivity.class);
+        Intent intent = new Intent(MainActivity.this, InformationActivity.class);
         startActivity(intent);
     }
 }
